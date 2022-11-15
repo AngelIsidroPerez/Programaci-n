3fa09d5d2a033aff5a042e75d0b25f33f0fd52c7
Ángel Isidro Pérez <20198752@itla.edu.do>
2022-11-14 21:44:00 -0400

    Añadí nuevo programa.

--- Programas Prácticos/P 2.12.c
+++ Programas Prácticos/P 2.12.c
@@ -1,21 +1,25 @@
-#include <stdio.h>
-#include <math.h>
+#include <math.h>  //Libreria
+#include <stdio.h> //Libreria
+
 /* Igualdad de expresiones.
 El programa, al recibir como datos T, P y N, comprueba la igualdad de
 una expresión determinada.
 T, P y N: variables de tipo entero. */
-void main(void)
+int main(void) // Es el punto de entrada al programa
 {
-int T, P, N;
-printf("Ingrese los valores de T, P y N: ");
-scanf("%d %d %d", &T, &P, &N);
-if (P != 0)
-{
-if (pow(T / P, N) == (pow(T, N) / pow(P, N))
-printf("\nSe comprueba la igualdad");
-else
-printf("\nNo se comprueba la igualdad");
-}
-else
-printf("\nP tiene que ser diferente de cero");
+    int P, N; // Define variables numericas con numeros de tipo entero
+    float T;
+    printf("Ingrese los valores de T, P y N: "); // Presenta en la pantalla algun
+                                                // dato
+    scanf("%d %d %d", &T, &P, &N); // Lee caracteres ingresados desde el teclado
+    if (P != 0) /* Se utiliza para establece una condicion, si es verdadera
+    se ejecutaran las siguientes instrucciones */
+    {
+        if (pow(T / P, N) == (pow(T, N) / pow(P, N)))
+        printf("\nSe comprueba la igualdad");
+        else
+        printf("\nNo se comprueba la igualdad");
+    } else // Si la condicion es falsa pasa a esta linea de codigo
+        printf("\nP tiene que ser diferente de cero"); /* Luego presentando este
+        mensaje por pantalla */
 }
